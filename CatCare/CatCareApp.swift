b367b94c19c5f6ceb5a00fa983cb3745fd7b887d
WajdButterfly <wajd2828@gmail.com>
2024-09-29 17:11:45 +0300

    Initial Commit

---
+++ CatCare/CatCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCareApp.swift
+//  CatCare
+//
+//  Created by Wajd on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct CatCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
