240cbababa2db3acaa556303795813228becaadc
alasirimaya <alasirimaya@gmail.com>
2024-10-02 16:29:35 +0300

    hi

--- CatCare/CareOverView.swift
+++ CatCare/CareOverView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct CareOverView: View {
-    
+    @State private var isFirstFoodPawFilled = false
+    @State private var isSecondFoodPawFilled = false
+    @State private var isWaterPawFilled = false
+    @State private var isLitterboxPawFilled = false
+
     var body: some View {
         
         ZStack{
@@ -50,7 +54,7 @@ struct CareOverView: View {
   VStack( spacing:5) {
       Text("CatName")
           .offset(x:-50,y:-10)
-          .foregroundColor(.white)
+          .foregroundColor(.black)
           //.font(.system(size: 15))
       HStack {
   Text("Food")
@@ -58,15 +62,28 @@ struct CareOverView: View {
 .foregroundColor(.black)
  .frame(maxWidth: .infinity, alignment: .leading)
  .padding(.leading, 15)
+          
   HStack(spacing: 5) {
- Image(systemName: "pawprint.fill")
- .foregroundColor(.black)
- Image(systemName: "pawprint.fill")
- .foregroundColor(.black)
+      
+      Image(systemName: isFirstFoodPawFilled ? "pawprint.fill" : "pawprint")
+         .foregroundColor(.black)
+      .onTapGesture {
+      isFirstFoodPawFilled.toggle()
+     }
+      Image(systemName: isSecondFoodPawFilled ? "pawprint.fill" : "pawprint")
+         .foregroundColor(.black)
+          .onTapGesture {
+              
+    if isFirstFoodPawFilled {
+      isSecondFoodPawFilled.toggle()//this makes sure you cant click the sceonde paw if the first one isnt clicked
    }
- .frame(maxWidth: .infinity, alignment: .center)
-  }
-  .padding(.top, 5)
+                                }
+     .opacity(isFirstFoodPawFilled ? 1.0 : 0.5) //this makes the seconde paw gray if not clicked on the first
+      
+                }
+   .frame(maxWidth: .infinity, alignment: .center)
+                                 }
+ .padding(.top, 5)
 
  Rectangle()
 .fill(Color.white)
@@ -77,14 +94,18 @@ struct CareOverView: View {
  .foregroundColor(.black)
 .frame(maxWidth: .infinity, alignment: .leading)
  .padding(.leading, 15)
+          
  HStack(spacing: 5) {
-  Image(systemName: "pawprint.fill")
-  .foregroundColor(.black)
+     Image(systemName: isWaterPawFilled ? "pawprint.fill" : "pawprint")
+ .foregroundColor(.black)
+ .onTapGesture {
+ isWaterPawFilled.toggle()
   }
-   .frame(maxWidth: .infinity, alignment: .center)
-}
-  .padding(.top, 5)
-
+    }
+          
+.frame(maxWidth: .infinity, alignment: .center)
+  }
+.padding(.top, 5)
                 
 Rectangle()
 .fill(Color.white)
@@ -98,17 +119,20 @@ Rectangle()
 .frame(maxWidth: .infinity, alignment: .leading)
  .padding(.leading, 15)
  
-Image(systemName: "pawprint")
-.foregroundColor(.black)
-
- .frame(maxWidth: .infinity, alignment: .center)
-   }
-  .padding(.top, 5)
+Image(systemName: isLitterboxPawFilled ? "pawprint.fill" : "pawprint")
+   .foregroundColor(.black)
+   .onTapGesture {
+ isLitterboxPawFilled.toggle()
  }
-.padding(.top, 15)
- )
-                
+  .frame(maxWidth: .infinity, alignment: .center)
+      }
+      .padding(.top, 5)
   }
+  .padding(.top, 15)
+)
+}
+
+  
             
       .padding(.top, 150)
       .position(x: 200, y: 150)
