0334ff6727892ca3f4d6c4b38ae01646cefdc630
Dalsaya <alshdeemah343@gmail.com>
2024-10-03 14:39:39 +0300

    s

--- CatCare/SetSchedule.swift
+++ CatCare/SetSchedule.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct SetSchedule: View {
-    @Binding var Utasks: [Task] 
     @State private var wakeUp = Date.now
     @State private var vaccinationDate = Date.now
 
@@ -196,9 +195,9 @@ struct TimeView: View {
 }
 
 struct CatNeedsView_Previews: PreviewProvider {
-    @State static var mockTasks: [Task] = []
     static var previews: some View {
-        SetSchedule(Utasks: $mockTasks)
+        SetSchedule()
     }
 }
+
 
